082947de73cae7805d6d20690b704ef978825fc8
Emil Andreasyan <emil.andreasyan@yahoo.com>
2019-12-13 02:04:36 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,13 +2,13 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/food' do
+  get '/food_form' do
     erb :food_form
   end
 
   # Add your post route and action below
   
-  get '/food/:name/:favorite_food' do
+  get '/food' do
     "My name is #{params[:name}, and I love #{params[:favorite_food}"
   end
 
